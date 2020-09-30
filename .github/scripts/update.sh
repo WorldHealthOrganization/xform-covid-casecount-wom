5ef91874032b9fe3f66c1d261c1507e9e22742a1
Ryan Hafen <rhafen@gmail.com>
2020-09-30 11:57:55 -0700

    Update org references

--- .github/scripts/update.sh
+++ .github/scripts/update.sh
@@ -3,7 +3,7 @@
 # Execution Script.
 ###############################################################################
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-SOURCE_GIT_URL="git@github.com:covid-open-data/xform-template.git"
+SOURCE_GIT_URL="git@github.com:WorldHealthOrganization/xform-template.git"
 TMP_DIR=$(mktemp -d)
 
 if git clone "${SOURCE_GIT_URL}" "${TMP_DIR}"; then
